0c99d270cdf66aa74852402ea368d165cc5924f1
Max F. Albrecht <1@178.is>
2014-04-22 21:31:43 +0200

    permissions: use better example data, embed directly in .js for static version

--- permissions/permissions-ui-react.jsx
+++ permissions/permissions-ui-react.jsx
@@ -303,3 +303,44 @@ React.renderComponent(
     redirectUrl="/permissions/edit?_action=view&amp;media_resource_id=5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb" />,
   document.getElementById('ux-permissions')
 );
+
+// import data for building static version
+var PERMISSIONS_JSON = {
+    "public": {
+        "view": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"],
+        "edit": [],
+        "download": []
+    },
+    "you": {
+        "view": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"],
+        "name": "Admin, Adam",
+        "id": "927ba041-9464-4e74-9823-c02d03fdfa5c",
+        "download": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"],
+        "edit": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"],
+        "manage": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"]
+    },
+    "users": [{
+        "id": "08ba1f4f-0522-4a77-b087-4f3d1dd94532",
+        "name": "Normalo, Normin",
+        "view": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"],
+        "download": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"],
+        "edit": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"],
+        "manage": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"]
+    }
+    ],
+    "groups": [{
+        "id": "540e224a-6dff-4fb5-83ee-655325a79081",
+        "name": "Bachelor Design - Vertiefung Visuelle Kommunikation - HS 2010 (DDE_FDE_BDE_VVK_10H.studierende)",
+        "view": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"],
+        "download": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"],
+        "edit": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"]
+    }
+    ],
+    "applications": [{
+        "id": "a-fancy-application",
+        "description": "An API application for testing",
+        "view": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"],
+        "download": ["5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb"]
+    }
+    ]
+};
