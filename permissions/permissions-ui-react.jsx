b540e1eb538408f92f6d59eebae28a15b76a33c0
Max F. Albrecht <1@178.is>
2014-04-23 02:23:20 +0200

    permissions: add config and use it in PermissionsSubjectGroup etc

--- permissions/permissions-ui-react.jsx
+++ permissions/permissions-ui-react.jsx
@@ -86,16 +86,18 @@ var PermissionsSettings = React.createClass({
   render: function () {
     
     // build list of subjectGroups from data
+    var config = this.props.config;
     var data = this.props.permissions;
     var subjectGroups = [];
-    // TODO: ordering???
-    Object.keys(data.subjectGroups).forEach(function (group) {
+    config.subjectGroups.forEach(function (group) {
       subjectGroups.push(
         <PermissionsSubjectGroup
-          subjects={data.subjectGroups[group]}
-          name={group}
-          title={group[0].toUpperCase().concat(group.slice(1))} // TODO: from preset
-          icon="icon-privacy-private-alt" />
+          config={config}
+          group={group}
+          subjects={data.subjectGroups[group.id]}
+          name={group.id}
+          title={group.name}
+          icon={group.icon} />
       );
     });
     
@@ -106,6 +108,13 @@ var PermissionsSettings = React.createClass({
         </h3>
         <div className="ui-rights-management">
           {subjectGroups}
+          <PermissionsSubjectGroup
+            config={config}
+            ref="primarySubject"
+            subjects={[config.primarySubject.entry]}
+            name={config.primarySubject.id}
+            title={config.primarySubject.name}
+            icon={config.primarySubject.icon} />
         </div>
       </div>
     );
@@ -114,6 +123,48 @@ var PermissionsSettings = React.createClass({
 
 var PermissionsSubjectGroup = React.createClass({
   render: function () {
+    
+    
+    var config = this.props.config;
+    
+    // build list of subjects from data
+    var subjects = [];
+    this.props.subjects.forEach(function (subject) {
+      subjects.push(
+        <PermissionsSubject
+          user={subject.id}
+          name={subject.name || subject.id}
+          config={config} />
+      );
+    });
+    
+    var tableHeaderRights = [];
+    config.rights.forEach(function (right) {
+      tableHeaderRights.push(
+        <td className="ui-rights-check-title">{right.name}</td>
+      );
+    });
+    
+    // TODO: config. >component?
+    var adder = this.props.group ? this.props.group.adder : null;
+    var subjectAdder = function () {
+      if (adder) {
+        return (
+            <div className="ui-add-subject ptx row" id="add{this.props.name.capitalize}">
+              <div className="col1of3">
+                <input autoComplete="off" 
+                       className="small block ui-autocomplete-input"
+                       name="user" 
+                       placeholder={adder.hint}
+                       type="text"
+                />
+                <span className="ui-helper-hidden-accessible" aria-live="polite" role="status"></span>
+              </div>
+            </div>
+        );
+      }
+    }
+    
     return (
       <div className={"ui-rights-management-"+this.props.name}>
         <div className="ui-rights-body">
@@ -124,30 +175,14 @@ var PermissionsSubjectGroup = React.createClass({
                   {this.props.title} <i className={this.props.icon}></i>
                 </td>
                 <td className="ui-rights-role-title">Berechtigung</td>
-                <td className="ui-rights-check-title">Betrachten</td>
-                <td className="ui-rights-check-title">Original exportieren</td>
-                <td className="ui-rights-check-title">Metadaten editieren</td>
-                <td className="ui-rights-check-title">Berechtigungen verwalten</td>
+                {tableHeaderRights}
               </tr>
             </thead>
             <tbody>
-              <PermissionsSubject
-                user="08ba1f4f"
-                name="Normalo, Normin"
-              />
+              {subjects}
             </tbody>
           </table>
-          <div className="ui-add-subject ptx row" id="add{this.props.name.capitalize}">
-            <div className="col1of3">
-              <input autoComplete="off" 
-                     className="small block ui-autocomplete-input"
-                     name="user" 
-                     placeholder="Name der Person"
-                     type="text"
-              />
-              <span className="ui-helper-hidden-accessible" aria-live="polite" role="status"></span>
-            </div>
-          </div>
+          {subjectAdder()}
         </div>
       </div>
     );
@@ -159,7 +194,18 @@ var PermissionsSubject = React.createClass({
     var user = {
       id: this.props.user,
       name: this.props.name
-    }
+    };
+    
+    var rightsRow = [];
+    this.props.config.rights.forEach(function (right) {
+      rightsRow.push(
+        <PermissionCheckBox
+          userSelection={true} // TODO: set from data
+          name={right}
+          title={right} />
+      );
+    });
+
     return (
       <tr data-id="08ba1f4f-0522-4a77-b087-4f3d1dd94532"
         data-is-current-user-group=""
@@ -180,26 +226,7 @@ var PermissionsSubject = React.createClass({
         
         <PermissionsPresetSelector/>
         
-        <PermissionCheckBox
-          userSelection={true}
-          name="view"
-          title="Betrachten"
-        />
-        <PermissionCheckBox
-          userSelection={true}
-          name="export"
-          title="Export original"
-        />
-        <PermissionCheckBox
-          userSelection={false}
-          name="edit"
-          title="Edit metadata"
-        />
-        <PermissionCheckBox
-          userSelection={false}
-          name="manage"
-          title="Manage permissions"
-        />
+        {rightsRow}
         
       </tr>
     );
@@ -271,12 +298,14 @@ var PermissionCheckBox = React.createClass({
 
 var Permissions = React.createClass({
   render: function () {
-    // some light data mangling: seperate "you" and "subjects"
+    // some light data mangling: seperate "public", "you" and "subjectGroups"
     var data = this.props.permissions
     var permissions = {};
     permissions.subjectGroups = data;
     permissions.currentUser = data.you;
-    delete permissions.subjects.you;
+    permissions.public = data.public;
+    delete permissions.subjectGroups.you;
+    delete permissions.subjectGroups.public;
     
     return (
         <form id="ui-rights-management"
@@ -286,11 +315,66 @@ var Permissions = React.createClass({
         >
           <PermissionsOverview permissions={permissions}/>
           <hr className="separator light mvl"/>
-          <PermissionsSettings permissions={permissions}/>
+          <PermissionsSettings permissions={permissions} config={this.props.config}/>
         </form>
     );
   }
 });
+
+// config
+var PERMISSIONS_CONFIG_JSON = {
+  "rights": [
+    {
+      "id": "view",
+      "name": "Betrachten"
+    },
+    {
+      "id": "download",
+      "name": "Original exportieren"
+    },
+    {
+      "id": "edit",
+      "name": "Metadaten editieren"
+    },
+    {
+      "id": "manage",
+      "name": "Berechtigungen verwalten"
+    }
+  ],
+  "primarySubject": {
+    "id": "public",
+    "name": "Öffentlichkeit",
+    "entry": {
+      "id": "Internet",
+    },
+    "overrides": true,
+    "iconClass": "icon-privacy-open",
+    "allowed": [ "view", "download" ]
+  },
+  "subjectGroups": [ 
+    {
+      "id": "users",
+      "name": "Benutzer",
+      "iconClass": "icon-privacy-private-alt",
+      "adder": { "hint": "Name der Person" },
+      "allowed": [ "view", "download", "edit", "manage"]
+    },
+    {
+      "id": "groups",
+      "name": "Gruppen",
+      "iconClass": "icon-privacy-group-alt",
+      "adder": { "hint": "Name der Gruppe" },
+      "allowed": [ "view", "download", "edit" ]
+    },
+    {
+      "id": "applications",
+      "name": "API-Applikationen",
+      "iconClass": "fa fa-flask",
+      "adder": { "hint": "Name der Applikation" },
+      "allowed": [ "view", "download" ]
+    }
+  ]
+};
 
 // import data for building static version
 var PERMISSIONS_JSON = {
@@ -341,6 +425,7 @@ var PERMISSIONS_JSON = {
 React.renderComponent(
   <Permissions
     permissions={PERMISSIONS_JSON}
+    config={PERMISSIONS_CONFIG_JSON}
     isManageable={true}
     redirectUrl="/permissions/edit?_action=view&amp;media_resource_id=5b8a97e9-84a2-46a9-b0f3-7c59af3fc4cb" />,
   document.getElementById('ux-permissions')
